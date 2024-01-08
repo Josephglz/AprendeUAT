61c79100057f8593b01c82d29fd715dfe3b7a385
Josephglz <joseph220199@gmail.com>
2024-01-07 19:00:33 -0600

    feat: advisory route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage) },
+  { path: 'asesorias', loadComponent: () => import('./pages/advisory/advisory.page').then((m) => m.AdvisoryPage) },
 ];
