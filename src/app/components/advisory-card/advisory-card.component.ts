bc39ec97cacf55ff84d2faa909b50a1ba61bd2a8
Josephglz <joseph220199@gmail.com>
2024-01-10 16:24:23 -0600

    feat: Advisory card component added for load data

---
+++ src/app/components/advisory-card/advisory-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advisory-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './advisory-card.component.html',
+  styleUrl: './advisory-card.component.css',
+})
+export class AdvisoryCardComponent {
+
+}
