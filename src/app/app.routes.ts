7322a4d242aa8094454616df6644c5bf1b438c79
Josephglz <joseph220199@gmail.com>
2024-01-10 16:24:06 -0600

    refactor: routes structure styled

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage) },
-  { path: 'asesorias', loadComponent: () => import('./pages/advisory/advisory.page').then((m) => m.AdvisoryPage) },
+  {
+    path: '',
+    title: 'AprendeUAT',
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage)
+  },
+  {
+    path: 'asesorias',
+    title: 'AprendeUAT - Asesorias',
+    loadComponent: () => import('./pages/advisory/advisory.page').then((m) => m.AdvisoryPage)
+  },
 ];
